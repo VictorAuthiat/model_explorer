7433ab0785a521e23981d6cfa880fe3457786720
Victor <authiatv@gmail.com>
2024-04-22 19:36:22 +0200

    Set root to models index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 ModelExplorer::Engine.routes.draw do
+  root to: "models#index"
+
   resources :models, only: [:index, :show]
   resources :exports, only: [:create]
 end
